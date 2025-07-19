361d966de78c80dc5267387603e5f216c08f94ad
edwardxiaoyicong <yxiaoaz@connect.ust.hk>
2025-07-19 17:36:00 +0800

    commit

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# API Keys
-JINA_API_KEY = os.getenv('JINA_API_KEY')  # From https://jina.ai/reader
-FIRECRAWL_API_KEY = os.getenv('FIRECRAWL_API_KEY')  # From https://firecrawl.dev
-
-# Service Selection
-USE_JINA = True  # Set False to use Firecrawl instead
