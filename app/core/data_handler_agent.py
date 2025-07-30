d104c50a596732aa6c6ff4a60f570a6c35776441
unknown <yicong.xiao@u.nus.edu>
2025-07-30 12:19:26 +0800

    start working on data handler agent

---
+++ app/core/data_handler_agent.py
@@ -0,0 +1,11 @@
+from app.models.base import Base
+from app.services.storage.db_controller import DBController
+from app.services.storage.utils import session_scope
+from app.services.storage.engine import engine
+
+
+class DataHandlerAgent:
+    def __init__(self):
+        self.db_controller = DBController(engine=engine)
+    
+    
