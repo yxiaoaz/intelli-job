90d8917e70cc3c3b2217b5fa1a341ddf372b4b30
unknown <yicong.xiao@u.nus.edu>
2025-08-06 09:58:25 +0800

    added embedding generation to crawler pipeline, but need to handle openai.apirequest time out

--- app/services/storage/engine.py
+++ app/services/storage/engine.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 
 DATABASE_URL = "sqlite:///test.db"  # Or your specific database URL
+DATABASE_URL = "postgresql+psycopg2://postgres:HelloWorld20010401@localhost:5432/intelli_job_db" # "postgresql+psycopg2://user:password@hostname:port/database_name"
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True for logging SQL statements
