469dae1b5ffe1c02cddb04630d94a18ddbb1423d
edwardxiaoyicong <yxiaoaz@connect.ust.hk>
2025-09-11 11:48:30 +0800

    commit

--- run_crawler.py
+++ run_crawler.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     process = CrawlerProcess(settings)
     process.crawl(ShixisengGraduateSpider)
     process.crawl(ShixisengInternSpider)
-    #process.crawl(ZhilianSpider)
-    #process.crawl(WelcomeToTheJungleSpider)
-    #process.crawl(CTGoodJobSpider)
+    process.crawl(ZhilianSpider)
+    process.crawl(WelcomeToTheJungleSpider)
+    process.crawl(CTGoodJobSpider)
     process.start()  # the script will block here until all crawling jobs are finished
